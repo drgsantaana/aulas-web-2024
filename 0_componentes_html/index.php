1703bbf31a89b3a7191aec10e25efd0a296e2550
Daniel Reis <113141830+drgsantaana@users.noreply.github.com>
2024-10-26 01:05:28 -0300

    feat

--- 0_componentes_html/index.php
+++ 0_componentes_html/index.php
@@ -51,36 +51,69 @@
             <input type="hidden" name="hddCodigo" value="123456" />
             <table border="1px" cellspacing="4px" cellpadding="5px">
                 <tr>
-                    <td colspan="2">
-                        Cadastro de cliente
-                    </td>
+                    <td colspan="2">Cadastro de cliente</td>
                 </tr>
                 <!-- componente text -->
                 <tr>
-                    <td>
-                        Codigo:
-                    </td>
+                    <td width="15%">Codigo:</td>
                     <td>
                         <input type="text" name="txtCodigo" value="098765" readonly="true">
                     </td>
                 </tr>
                 <!-- componente text -->
                 <tr>
-                    <td>
-                        Nome:
-                    </td>
+                    <td width="15%">Nome:</td>
                     <td>
                         <input type="text" id="txtNome" name="txtNome" placeholder="Nome Completo" size="100" value="" maxlength="80">
                     </td>
                 </tr>
                 <!-- componente password -->
                 <tr>
-                    <td>Senha:</td>
+                    <td width="15%">Senha:</td>
                     <td><input type="password" name="txtSenha" value="coxinha123" maxlength="10" disabled="true"></td>
                 </tr>
+                <!-- componente email -->
                 <tr>
-                    <td>Email</td>
+                    <td width="15%">Email:</td>
                     <td><input type="email" name="txtEmail" placeholder="email@exemplo.com.br" size="100" maxlength="100"></td>
+                </tr>
+                <!-- componente data -->
+                <tr>
+                    <td width="15%">Data de Nascimento:</td>
+                    <td><input type="date" name="txtDataNasc"></td>
+                </tr>
+                <!-- componente arquivo -->
+                <tr>
+                    <td>Envie sua foto:</td>
+                    <td><input type="file" name="txtArquivo"></td>
+                </tr>
+                <!-- componente radio -->
+                <tr>
+                    <td width="15%">Renda:</td>
+                    <td>
+                        <input type="radio" name="rdbRenda" id="rdbMenosQue800" value="1" checked="POBRE">
+                        <label for="rdbMenosQue800">Menos que R$800</label>
+                        <input type="radio" name="rdbRenda" id="rdbMaisQue800" value="2">
+                        <label for="rdbMaisQue800">Mais que R$800</label>
+                        <input type="radio" name="rdbRenda" id="rdbEstagiario" value="3">
+                        <label for="rdbEstagiario">Pago pra trabalhar</label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="15%">
+                        Quais seu estilo de musicas favoritas
+                    </td>
+                    <td>
+                        <input type="checkbox" name="chkLofi" value="lofi"> Lofi
+                        <br>
+                        <input type="checkbox" name="chkRock" value="Rock"> Rock
+                        <br>
+                        <input type="checkbox" name="chkSamba" value="Samba"> Samba
+                        <br>
+                        <input type="checkbox" name="chkPagode" value="Pagote"> Pagode
+
+                    </td>
                 </tr>
                 <tr>
                     <td colspan="2" align="center">
--- 0_componentes_html/processa.php
+++ 0_componentes_html/processa.php
@@ -1,24 +1,67 @@
 <?php
 
 //captura de variaveis
+
+use function PHPSTORM_META\map;
+
 $codigoEscondido = $_POST['hddCodigo'];
 $codigo = $_POST['txtCodigo'];
-$nome = trim($_POST['txtNome']);  //trim para cortar os espaços vazios nas laterais
+$nome = 'Nome nao informado';
+if (!empty($_POST['txtNome'])) {
+	$nome = trim($_POST['txtNome']);  //trim para cortar os espaços vazios nas laterais
+}
+
 $senha = 'Senha nao informada';
 if (isset($_POST[$senha])) {  //verifica se variavel existe na url
 	$senha = $_POST['txtSenha'];
 }
+
 $email = trim($_POST['txtEmail']);
+
+$dataNascimento = 'Data de nascimento nao informada ou invalida';
+if (!empty($_POST['txtDataNasc'])) {  //verifica se variavel existe na url
+	$dataNascimento = date('d/m/Y', strtotime($_POST['txtDataNasc']));
+}
+
+$arquivo = $_FILES['txtArquivo'];
+
+$renda = 'Renda nao informada';
+if (!empty($_POST['rdbRenda'])) {  //verifica se variavel existe na url
+	$renda = $_POST['rdbRenda'];
+}
+
+$musica = [];
+if (isset($_POST['chkLofi'])) {
+	array_push($musica, 'Lofi');
+} elseif (isset($_POST['chkRock'])) {
+	$musica = 'Rock';
+} elseif (isset($_POST['chkSamba'])) {
+	$musica = 'Samba';
+} elseif (isset($_POST['chkPagode'])) {
+	$musica = 'Pagode';
+}
+
+
 
 //exibicao de variaveis
 echo 'Codigo escondido: ' . $codigoEscondido . '<br>';
 echo 'Codigo: ' . $codigo . '<br>';
-
-if (empty($name)) {  //verifica se esta vazio
-	echo 'Nome nao informado <br>';
-} else {
-	echo 'Nome: ' . $nome . '<br>';
-}
-
+echo 'Nome: ' . $nome . '<br>';
 echo 'Senha: ' . $senha . '<br>';
 echo 'Email: ' . $email . '<br>';
+echo 'Data de nascimento: ' . $dataNascimento . '<br>';
+// print_r($arquivo);  //print de um array
+echo 'Nome do arquivo: ' . $arquivo['name'] . ' - ' . ' Tamanho: ' . $arquivo['size'] . 'kb' . '<br>';
+
+if ($renda == 1) {
+	echo 'Renda: Não paga o alugel no fim do mes <br>';
+} elseif ($renda == 2) {
+	echo 'Renda: Já da para pagar o alugel no fim do mes <br>';
+} else {
+	echo 'Renda: Ala o cara trabalha de graça kkkkkkkkkkkkkkkkkkkkkkkkkkkkkk <br>';
+}
+
+echo 'Estilo Musical: ';
+for ($i = 0; $i < count($musica); $i++) {
+	echo $musica[$i];
+}
